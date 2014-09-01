7d820d318b189b90375976abbd06c5d0068e65ac
Nimbleguy <Happyboy431@gmail.com>
2014-08-31 22:06:18 -0400

    Added actual code

---
+++ src/aj/Java/SpaceSuit/Listners/SpaceSuitListner.java
@@ -0,0 +1,150 @@
+package aj.Java.SpaceSuit.Listners;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import aj.Java.SpaceSuit.Main;
+
+public class SpaceSuitListner implements Listener {
+	public static HashMap<String, Integer> timer = new HashMap<String, Integer>(20);
+	@EventHandler
+	public void playerMove(PlayerMoveEvent e){
+		Player p = e.getPlayer();
+		for(int world = 0; world < Main.worlds.size(); world++){
+			if(Main.debug){
+				Main.l.info(Main.worlds.get(world));
+			}
+			if(p.getWorld().getName().equalsIgnoreCase(Main.worlds.get(world))){
+			//if(p.getWorld().getName().equalsIgnoreCase("world")){
+				if(Main.debug){
+					Main.l.info(Main.bubble);
+				}
+				boolean notNeedTank = nearBubble(p);
+				if(timer.get(p.getDisplayName()) != null){
+					if(timer.get(p.getDisplayName()) == 200){
+						notNeedTank = true;
+					}
+					else{
+						timer.put(p.getDisplayName(), timer.get(p.getDisplayName()) + 1);
+					}
+				}
+				//Do nothing if tank not needed
+				if(notNeedTank){
+					if(Main.debug){
+						Main.l.info("Oxy tank not needed");
+					}
+				}
+				//Oxygen Tank
+				else if((p.getInventory().getHelmet() != null && p.getInventory().getChestplate() != null && !Main.full) || (p.getInventory().getHelmet() != null && p.getInventory().getChestplate() != null && p.getInventory().getLeggings() != null && p.getInventory().getBoots() != null && Main.full)){
+					if(Main.debug){
+						Main.l.info(Material.matchMaterial(Main.tank).toString());
+					}
+					if(p.getInventory().getItem(Main.tankslot).getType().compareTo(Material.matchMaterial(Main.tank)) == 0){
+						if(Main.debug){
+							Main.l.info(Material.matchMaterial(Main.mask).toString());
+						}
+						if(p.getInventory().getItem(Main.helslot).isSimilar(new ItemStack(Material.matchMaterial(Main.mask)))){
+							ItemStack c = p.getInventory().getChestplate();
+							ItemMeta m = c.getItemMeta();
+							if(m.hasLore()){
+								List<String> lore = m.getLore();
+								if(isInt(lore.get(0))){
+									int i = Integer.valueOf(lore.get(0));
+									if(i > 0){
+										i--;
+										lore.clear();
+										lore.add(Integer.toString(i));
+										m.setLore(lore);
+										c.setItemMeta(m);
+										p.getInventory().setChestplate(c);
+									}
+									else{
+										p.damage(Main.damage);
+									}
+								}
+							}
+							else{
+								List<String> lore = m.getLore();
+								lore.add(Main.oxygen + "");
+								m.setLore(lore);
+							}
+						}
+						else{
+							p.damage(Main.damage);
+						}
+					}
+					else{
+						p.damage(Main.damage);
+					}
+				}
+				else{
+					p.damage(Main.damage);
+				}
+			}
+		}
+	}
+	@EventHandler
+	public void onPlayerUse(PlayerInteractEvent event){
+		if(event.hasItem() && event.hasBlock()){
+			if(new ItemStack(event.getClickedBlock().getType()).isSimilar(new ItemStack(Material.matchMaterial(Main.refill)))){
+				if(event.getItem().getType().compareTo(Material.matchMaterial(Main.tank)) == 0){
+					if(event.getItem().getItemMeta().hasLore()){
+						List<String> lore = event.getItem().getItemMeta().getLore();
+						lore.clear();
+						lore.add(Main.oxygen + "");
+						ItemMeta m = event.getItem().getItemMeta();
+						m.setLore(lore);
+						event.getItem().setItemMeta(m);
+						event.getPlayer().getInventory().setChestplate(event.getItem());
+						event.getPlayer().getInventory().setItemInHand(null);
+					}
+					else{
+						List<String> lore = new ArrayList<String>(1);
+						lore.add(Main.oxygen + "");
+						ItemMeta m = event.getItem().getItemMeta();
+						m.setLore(lore);
+						event.getItem().setItemMeta(m);
+						event.getPlayer().getInventory().setChestplate(event.getItem());
+						event.getPlayer().getInventory().setItemInHand(null);
+					}
+				}
+				else if(event.getItem().isSimilar(new ItemStack(Material.matchMaterial(Main.mask)))){
+					event.getPlayer().getInventory().setHelmet(event.getItem());
+					event.getPlayer().getInventory().setItemInHand(null);
+				}
+			}
+		}
+	}
+	private boolean isInt(String s){
+		try{
+			Integer.parseInt(s);
+			return true;
+		}
+		catch(Exception e){
+			return false;
+		}
+	}
+	private boolean nearBubble(Player p){
+		int radius = Main.bubbleR;
+		for(int i = -radius; i <= radius; i++) {
+		    for(int j = -radius; j <= radius; j++) {
+		        for(int k = -radius; k <= radius; k++) {
+		            if(p.getLocation().getBlock().getRelative(i, j, k).getType().toString().equalsIgnoreCase(Main.bubble)){
+		                return true;
+		            }
+		        }
+		    }
+		}
+		return false;
+	}
+}
---
+++ src/aj/Java/SpaceSuit/Main.java
@@ -0,0 +1,94 @@
+package aj.Java.SpaceSuit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import aj.Java.SpaceSuit.Listners.SpaceSuitListner;
+
+public class Main extends JavaPlugin {
+	public static Logger l;
+	public static boolean debug = true;
+	public static List<String> worlds = new ArrayList<String>();
+	public static String mask = "GLASS";
+	public static String tank = "CACTUS";
+	public static String refill = "SPONGE";
+	public static String bubble = "SPONGE";
+	public static int oxygen = 9999;
+	public static int bubbleR = 5;
+	public static boolean full = true;
+	public static int damage = 4;
+	public static int helslot = 103;
+	public static int tankslot = 102;
+	@Override
+	public void onEnable(){
+		this.saveDefaultConfig();
+		debug = this.getConfig().getBoolean("debug");
+		worlds = this.getConfig().getStringList("worlds");
+		mask = this.getConfig().getString("mask");
+		tank = this.getConfig().getString("tank");
+		refill = this.getConfig().getString("refill");
+		full = this.getConfig().getBoolean("full");
+		bubble = this.getConfig().getString("bubble");
+		oxygen = this.getConfig().getInt("oxygen");
+		bubbleR = this.getConfig().getInt("bradius");
+		damage = this.getConfig().getInt("damage");
+		helslot = this.getConfig().getInt("helslot");
+		tankslot = this.getConfig().getInt("tankslot");
+		this.getServer().getPluginManager().registerEvents(new SpaceSuitListner(), this);
+		if(this.getConfig().getBoolean("crafts")){
+			addRecipes();
+		}
+		if(debug){
+			for(int i = 0; i < worlds.size(); i++){
+				getLogger().info(worlds.get(i));
+			}
+		}
+		l = getLogger();
+		getLogger().info("SpaceSuit is ready to breath!");
+	}
+	@Override
+	public void onDisable(){
+		getLogger().info("*choke*need air*choke*");
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("ssreload")){
+			if(sender.hasPermission("spacesuit.reload")){
+				this.reloadConfig();
+				debug = this.getConfig().getBoolean("debug");
+				worlds = this.getConfig().getStringList("worlds");
+				mask = this.getConfig().getString("mask");
+				tank = this.getConfig().getString("tank");
+				refill = this.getConfig().getString("refill");
+				full = this.getConfig().getBoolean("full");
+				bubble = this.getConfig().getString("bubble");
+				oxygen = this.getConfig().getInt("oxygen");
+				bubbleR = this.getConfig().getInt("bradius");
+				damage = this.getConfig().getInt("damage");
+				helslot = this.getConfig().getInt("helslot");
+				tankslot = this.getConfig().getInt("tankslot");
+				sender.sendMessage("SpaceSuit reloaded!");
+			}
+		}
+		return true; 
+	}
+	private void addRecipes(){
+		ShapedRecipe sponge = new ShapedRecipe(new ItemStack(Material.SPONGE, 1));
+		sponge.shape("CWC", "GIG", "RPR");
+		sponge.setIngredient('W', Material.WOOL);
+		sponge.setIngredient('C', Material.CACTUS);
+		sponge.setIngredient('G', Material.GOLD_BLOCK);
+		sponge.setIngredient('I', Material.IRON_BLOCK);
+		sponge.setIngredient('R', Material.REDSTONE);
+		sponge.setIngredient('P', Material.REDSTONE_BLOCK);
+		this.getServer().addRecipe(sponge);
+	}
+}
